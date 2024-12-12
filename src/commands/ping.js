a5017f7b9e92d731f4f5514e9a47313689437965
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-12-12 17:17:15 -0300

    fix: channel

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,24 +2,13 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
 const ping = require('ping');
 
-const ALLOWED_CHANNEL_ID = '1315464652490150020';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Pings the api url'),
     async execute(interaction) {
-      if (interaction.channelId !== ALLOWED_CHANNEL_ID) {
-            const errorEmbed = new EmbedBuilder()
-                .setColor('#232428')
-                .setDescription('Use commands on <#1315464652490150020> channel.');
 
-            await interaction.reply({ embeds: [errorEmbed], ephemeral: false });
-            setTimeout(() => {
-                interaction.deleteReply().catch(console.error);
-            }, 5000);
-            return;
-        }
         try {
             const host = 'api.audibert.rest';
             const res = await ping.promise.probe(host);
